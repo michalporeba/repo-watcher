596e7c0044a31b105c4d2b7b9909e65d56ee629f
Michał Poręba <michalporeba@gmail.com>
2024-02-26 18:40:09 +0000

    a

--- tests/integration/test-filesystem-cache.js
+++ tests/integration/test-filesystem-cache.js
@@ -4,26 +4,47 @@ import { createCache } from "../../src/cache-utils";
 
 describe("Test file system cache", () => {
   test("Ensure full value lifecycle is supported", async () => {
-    const config = { cache: { type: "fs" } };
+    const config = { type: "fs" };
     const cache = await createCache(config);
-
     const key = "test";
-    await cache.remove(key);
-    expect(await cache.get(key)).toBeUndefined();
-
     const data = { value: 42, label: "fourty two" };
-    await cache.set(key, data);
-    expect(await cache.get(key)).toMatchObject(data);
-
-    await cache.update(key, { label: "pedwardeg dau", translated: true });
     const translated = {
       value: 42,
       label: "pedwardeg dau",
       translated: true,
     };
+
+    await cache.remove(key);
+    expect(await cache.get(key)).toBeUndefined();
+
+    await cache.set(key, data);
+    expect(await cache.get(key)).toMatchObject(data);
+
+    await cache.update(key, { label: "pedwardeg dau", translated: true });
+
     expect(await cache.get(key)).toMatchObject(translated);
 
     await cache.remove(key);
     expect(await cache.get(key)).toBeUndefined();
+  });
+  test("Custom cache and key work too", async () => {
+    const primaryCache = await createCache({ type: "fs" });
+    const secondaryCache = await createCache({
+      type: "fs",
+      path: "cache/secondary",
+    });
+    const key = "path/like/key";
+    const data = { hello: "world" };
+
+    await primaryCache.remove(key);
+    await secondaryCache.remove(key);
+
+    expect(await primaryCache.get(key)).toBeUndefined();
+    expect(await secondaryCache.get(key)).toBeUndefined();
+
+    await secondaryCache.set(key, data);
+
+    expect(await secondaryCache.get(key)).toMatchObject(data);
+    expect(await primaryCache.get(key)).toBeUndefined();
   });
 });
